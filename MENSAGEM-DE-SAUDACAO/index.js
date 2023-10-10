5a3418d0111aac84574af802370bee3e222be0e5
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-10 13:53:43 -0300

    Primeiro algoritmo em nodeJS

--- MENSAGEM-DE-SAUDACAO/index.js
+++ MENSAGEM-DE-SAUDACAO/index.js
@@ -10,5 +10,8 @@ if(hora <= 11) {
 } else if (hora <= 17) {
     saudacao = "Boa tarde!"
 }else {
-    
+    saudacao = "Boa noite!"
 }
+
+console.log("Olá " + saudacao)
+console.log("Agora são " + hora + ' Horas ' + minutos + ' Minutos ' )
