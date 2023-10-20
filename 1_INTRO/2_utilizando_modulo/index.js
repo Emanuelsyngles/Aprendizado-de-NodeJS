3fa24c0432311d6c86940c7d6178502659376557
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-20 15:28:14 -0300

    Update index.js

--- 1_INTRO/2_utilizando_modulo/index.js
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -1,5 +1,9 @@
-const fs = required('fs') // file system
+const fs = require('fs') // file system
 
 fs.readFile('arquivo.txt', 'utf8', (err, data) => {
 
-})
+    if (err) {
+        console.log(err)
+    }
+    console.log(data);
+});
