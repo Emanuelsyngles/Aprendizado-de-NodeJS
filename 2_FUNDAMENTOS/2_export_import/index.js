7ef9a42601b1400f97bcb2aae546b80a1de99cb5
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-22 17:44:33 -0300

    Estudos do dia finalizados

---
+++ 2_FUNDAMENTOS/2_export_import/index.js
@@ -0,0 +1,5 @@
+const meuModulo = require('./meu_modulo')
+const soma = meuModulo.soma
+
+soma(12, 10)
+soma(6, 6)
--- 2_FUNDAMENTOS/2_export_import/index.mjs
+++
@@ -1,5 +0,0 @@
-const meuModulo = require('./meu_modulo')
-const soma = 
-
-soma(2, 3)
-soma(5, 10)
