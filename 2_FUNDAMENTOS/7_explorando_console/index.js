6d43f97cfea7d8da049ab2dc669f38d8e3ab6c7d
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-25 15:28:59 -0300

    Update index.js

--- 2_FUNDAMENTOS/7_explorando_console/index.js
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -16,3 +16,9 @@ console.count(`O valor de x é: ${x}, contagem: `)
 // variavel entre string
 
 console.log("O nome é %s, ele é programador", y)
+
+// Limpar o console
+
+setTimeout(() => {
+    console.clear()
+}, 2000);
