271c5de03b22e6c87e9954f4e47add1ca61ce537
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-28 20:16:05 -0300

    Pacote externo reinstalado

--- 2_FUNDAMENTOS/10_abstracao_input/index.js
+++ 2_FUNDAMENTOS/10_abstracao_input/index.js
@@ -1,1 +1,26 @@
-const inquire = require('inquirer')
+const inquirer = require('inquirer')
+
+inquirer
+   .prompt([
+   {
+    name: 'p1', 
+    message: 'Qual a primeira nota?'
+   }, 
+   {
+    name: 'p2', 
+    message: 'Qual a segunda nota?'
+   }
+])
+.then((answers) => {
+    console.log(answers)
+    const media = ((parseInt(answers.p1) + parseInt(answers.p2)) / 2)
+    console.log(`A média é: ${media}`)
+
+    if(media < 7 ) {
+        console.log('Você entrou em processo de recuperação.')
+    }else {
+        
+    }
+
+})
+.catch((err) => console.log(err))
