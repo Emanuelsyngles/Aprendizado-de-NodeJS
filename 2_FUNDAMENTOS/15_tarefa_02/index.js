9684cd84d997dc7fdb626ac3b199e5d81eed1ce6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-01 18:24:12 -0300

    Repassando códigos que treinei.

---
+++ 2_FUNDAMENTOS/15_tarefa_02/index.js
@@ -0,0 +1,14 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+    {
+        name:'name',
+        
+    },
+    {
+
+    }
+])
