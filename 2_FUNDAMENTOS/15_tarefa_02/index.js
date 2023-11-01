9cc1423fbd3a6005e2b7ded948a02632db97053e
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-01 18:41:21 -0300

    Tarefa de treino.

--- 2_FUNDAMENTOS/15_tarefa_02/index.js
+++ 2_FUNDAMENTOS/15_tarefa_02/index.js
@@ -1,14 +1,17 @@
-import chalk from "chalk";
-import inquirer from "inquirer";
-
 const inquirer = require('inquirer')
 
 inquirer.prompt([
     {
         name:'name',
-        
+        message: 'Qual o seu nome? '
     },
     {
-
+        name: 'idade',
+        message: 'Qual a sua idade?'
     }
-])
+]).then((answers) => {
+    const response = (`O nome do usuário é ${answers.name} e sua idade é de ${answers.idade}`)
+    console.log(chalk.bgYellow.black(response))
+})
+
+.catch((err) => console.log(err))
