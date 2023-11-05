1f10f9215bcb894f87e5c0f95c2b90df9dd9badd
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-05 18:12:57 -0300

    Módulo url.

--- 3_CORE_MODULES/3_url/index.js
+++ 3_CORE_MODULES/3_url/index.js
@@ -3,4 +3,7 @@ const address = 'https://www.meusite.com.br/catalogo?produtos=cadeira'
 const parsedUrl = new url.URL(address)
 
 console.log(parsedUrl.host)
-console.log()
+console.log(parsedUrl.pathname)
+console.log(parsedUrl.search)
+console.log(parsedUrl.searchParams)
+console.log(parsedUrl.searchParams.get('produtos'))
