213973dfeca92d06273f248833011895a16e44de
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-05 17:57:29 -0300

    HTML rodando.

--- 3_CORE_MODULES/2_retornando_html/index.js
+++ 3_CORE_MODULES/2_retornando_html/index.js
@@ -3,10 +3,9 @@ const http = require("http")
 const port = 3000
 
 const server = http.createServer((req, res) => {
-
-    res.write('Oi HTTP')
-    res.end()
-
+    res.statusCode = 200
+    res.setHeader('Contenty-Type', 'text/html')
+    res.end('<h1>Olá este é meu primeiro server com     html!</h1>')
 })
 
 
