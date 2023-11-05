5cc106a8864b9a25a049c3b5ab984d3efb04b87f
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-05 17:27:45 -0300

    Update index.js

--- 3_CORE_MODULES/1_http/index.js
+++ 3_CORE_MODULES/1_http/index.js
@@ -1,3 +1,15 @@
 const http = require("http")
 
-const port = 
+const port = 3000
+
+const server = http.createServer((req, res) => {
+
+    res.write('Oi HTTML')
+    res.end()
+
+})
+
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
