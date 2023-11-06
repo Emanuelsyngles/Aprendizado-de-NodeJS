3111a8298113e6f0411e15606c3caae4dfd2c661
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-06 14:50:20 -0300

    Update index.js

--- 3_CORE_MODULES/4_http_com_url/index.js
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -3,9 +3,19 @@ const http = require("http")
 const port = 3000
 
 const server = http.createServer((req, res) => {
+
+    const urlInfo = require('url').parse(req.url, true)
+    const name = urlInfo.query.name
+
     res.statusCode = 200
     res.setHeader('Contenty-Type', 'text/html')
-    res.end('<h1>Olá este é meu primeiro server com     html!<p>Testando atualização</p></h1>')
+
+
+    if(!name) {
+        res.end('<h1>Preencha seu nome:</h1><form method="GET"><input type="text" name="name"/><input type="submit" value="Enviar"/></form>')
+    }else {
+        res.end(`<h1>Seja bem-vindo(a) ${name}</h1>`)
+    }
 })
 
 
