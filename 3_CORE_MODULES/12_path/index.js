12059a7f273bbc11e814142b0b02cff0b8b61ed7
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-08 17:02:00 -0300

    Create index.js

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = "/relatorios/emanuel/relatorio1.pdf"
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
