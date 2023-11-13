4bb873112daabe44d6e5c1aaaed85597436a143b
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-13 18:08:30 -0300

    Estudando

--- 4_NPM_FUNDAMENTAL/3_instalando_pacote/index.js
+++ 4_NPM_FUNDAMENTAL/3_instalando_pacote/index.js
@@ -1,6 +1,9 @@
-const _ = require('loadash')
+import _ from 'lodash'
 
 const a = [1, 2, 3, 4, 5]
-const b = [2, 4, 6, 7, 9]
+const b = [2, 3, 5, 6, 8]
 
 const diff = _.difference(a, b)
+const diff2 = _.difference(b, a)
+
+console.log(diff, diff2)
