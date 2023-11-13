7525ff294e17af01ac62c37a1ceec53b062d6049
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-13 14:02:23 -0300

    Update index.js

--- 3_CORE_MODULES/13_path_absoluto/index.js
+++ 3_CORE_MODULES/13_path_absoluto/index.js
@@ -1,1 +1,16 @@
-const path = require("path")
+const path = require('path')
+
+// Path absoluto
+console.log(path.resolve('teste.txt'))
+
+//Formar o path
+const midfolder = 'relatorios'
+const fileName = 'Emanuel.txt'
+
+const finalpath = path.join('/', 'arquivos', midFolder, fileName)
+
+
+console.log(finalpath)
+
+
+
