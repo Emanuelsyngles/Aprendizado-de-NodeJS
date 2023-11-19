54a0527b19b448256293aefe8c55383a97323ef3
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-19 17:11:46 -0300

    middleware

---
+++ 6_EXPRESS/5_middleware/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const port = 3000 // variável ambiente
+
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`O servidor está alocado na porta ${port}`)
+})
+
