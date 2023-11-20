3b1755799d26d147bdf02549e3140296a30baad5
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-20 14:22:08 -0300

    Css com node

---
+++ 6_EXPRESS/9_colocando_css/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const port = 3000 // variável ambiente
+
+const path = require('path')
+
+const usersRoutes = require('./users')
+
+// ler o lobby
+app.use(
+    express.urlencoded({
+        extended:true,
+    }),
+)
+
+app.use(express.json())
+
+const basePath = path.join(__dirname, 'templates')
+
+app.use('/users', usersRoutes)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`O servidor está alocado na porta ${port}`)
+})
+
+
+
