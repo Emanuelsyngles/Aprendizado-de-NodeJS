18e872549ff2459ddc98df7eaddaccee8ba3a3c3
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-20 17:16:59 -0300

    ....

--- 6_EXPRESS/11_projeto/index.js
+++ 6_EXPRESS/11_projeto/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000 // variável ambiente
+const port = 5000 // variável ambiente
 
 const path = require('path')
 
@@ -26,8 +26,8 @@ app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
 })
 
-app.use(function(req, res, next) {
-    res.status(404).sendFile(`${basePath}/404.html`)
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/sapatos.html`)
 })
 
 app.listen(port, () => {
