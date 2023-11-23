f6e14442a3450c0165645d6fe4823b5320c19ebd
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-22 21:39:20 -0300

    ...

--- 7_TEMPLATE_ENGINE/4_condicionais/index.js
+++ 7_TEMPLATE_ENGINE/4_condicionais/index.js
@@ -9,14 +9,16 @@ app.set('view engine', 'handlebars')
 app.get('/', (req, res) =>{
 
     const palavra = 'Teste'
+    
+    const auth = true
 
     const user = { 
         name: 'Emanuel',
         surname: 'Leal',
-        age: 21
+        age: 21,
     }
 
-    res.render('home', {user: user, palavra})
+    res.render('home', {user: user, palavra, auth})
 })
 
 app.listen(3000, ()=> {
