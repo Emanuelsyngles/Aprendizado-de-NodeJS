46bc94325e2418729b0f80a046885d9dd14278db
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-11-23 17:04:59 -0300

    else.

---
+++ 7_TEMPLATE_ENGINE/5_else/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashbord', (req, res) => {
+    res.render('dashbord')
+})
+
+app.get('/', (req, res) =>{
+
+    const palavra = 'Teste'
+
+    const auth = true
+
+    const user = { 
+        name: 'Emanuel',
+        surname: 'Leal',
+        age: 21,
+    }
+
+    res.render('home', {user: user, palavra, auth})
+})
+
+app.listen(3000, ()=> {
+    console.log('App funcionando!')
+})
