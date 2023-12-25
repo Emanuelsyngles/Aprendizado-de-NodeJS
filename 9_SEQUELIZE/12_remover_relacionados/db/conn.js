95061eccf000eeaa42c8f6f33aff6519f020c0d4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-12-24 22:39:59 -0300

    Excluindo registro relacionado.

---
+++ 9_SEQUELIZE/12_remover_relacionados/db/conn.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodesequelize2', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+//try {
+
+   // sequelize.authenticate()
+   // console.log('Sequelize conectado com sucesso!')
+
+//}catch(err) {
+    // console.log('Não foi possivel conectar:', error)
+//}
+
+//module.exports = sequelize
+
+module.exports = sequelize
