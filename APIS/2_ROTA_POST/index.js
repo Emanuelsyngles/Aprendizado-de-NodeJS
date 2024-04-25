97bb727e23f7f15dc5abfe1dc724d8642b0a9f28
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-25 19:24:22 -0300

    APIS

--- APIS/2_ROTA_POST/index.js
+++ APIS/2_ROTA_POST/index.js
@@ -10,6 +10,22 @@ app.use(
 app.use(express.json())
 
 // rotas - endpoints
+app.post('/createproduct', (req, res) => {
+
+    const name = req.body.name
+    const price = req.body.price
+
+    if(!name) {
+        res.status(422).json({message: 'O campo nome é obrigatório!' })
+        return
+    }
+
+    console.log(name)
+    console.log(price)
+
+    res.status(200).json({message: `O produto ${name} foi criado com sucesso!`})
+})
+
 app.get('/', (req, res) => {
 
     res.json({message: 'Primeira rota criada com sucesso!'})
